3b710cc724346ead23915627cb4e9ddfbab26f00
DunkRing <Chris31@live.dk>
2018-01-29 12:45:24 +0100

    studerende implementeret
    nemt

--- ClassRoom/Studerende.cs
+++ ClassRoom/Studerende.cs
@@ -9,6 +9,18 @@ namespace ClassRoom
     public class Studerende
     {
         private string _navn;
-        private int 
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        public Studerende(string Navn, int Fødselsmåned, int Fødselsdag)
+        {
+            Navn = _navn;
+            Fødselsmåned = _fødselsmåned;
+            Fødselsdag = _fødselsdag;
+        }
+
+        public string Navn { get; set; }
+        public int Fødselsmåned { get; set; }
+        public int Fødselsdag { get; set; }
     }
 }
