125a59480bb2bb8936470bb442e78add3c27a2ba
DunkRing <Chris31@live.dk>
2018-01-29 12:39:55 +0100

    studer
    sdsd

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        private string _navn;
+        private int 
+    }
+}
